a81c33d7bb1f3605a1b4fe8976540e87cf88ff41
JoynerM1426 <migue1409.mg@gmail.com>
2023-03-12 15:17:23 -0500

    advance

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,130 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const sendEmail = require('../utils/senEmail');
+const EmailCode = require('../models/EmailCode');
+const jwt = require('jsonwebtoken')
+
+const getAll = catchError(async(req, res) => {
+    const results = await User.findAll();
+    return res.json(results);
+});
+
+const create = catchError(async(req, res) => {
+    const {email, password, firstName, lastName, country, image, frontBaseUrl} = req.body;
+    const encripted= await bcrypt.hash(password,10)
+    const result = await User.create({email, password: encripted, firstName, lastName, country, image});
+    const code= require('crypto').randomBytes(32).toString("hex") //genera el codigo
+    const link= `${frontBaseUrl}/verify_email/${code}`;  //toma la base del front y le envia el codigo generado
+    await sendEmail ({
+      to:email,
+      subject:'verify your email',
+      html: 
+      `      <h1 style= color:gray>
+                  Hello ${firstName}
+             <h1>
+                <p>verify your email <p>
+                <p>go to your email<p>
+             <a href="${link}"> ${link} <a>
+     `
+    })
+    await EmailCode.create({code, userId: result.id}) //conexion con el modelo de emailsCode
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await User.findByPk(id);
+    if(!result) return res.sendStatus(404);
+    return res.json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: {id} });
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+  const { firstName, lastName, country, image} = req.body;
+    const { id } = req.params;
+    const result = await User.update(
+         { firstName, lastName, country, image} , //elimina actualizar el email y password
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
+
+const verifyEmail= catchError(async(req, res) => {
+  const {code}=   req.params;
+  const emailCode=  await EmailCode.findOne({where: { code}});
+  if(!emailCode) return res.status(401).json({message:"invalid code"})
+  await User.update({isVerified:  true}, {where: {id: emailCode.userId}})
+  await emailCode.destroy();
+  return res.json(emailCode);
+});
+const login = catchError(async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({where: {email}})
+    if(!user) return res.status(401).json({message: "invalid token"})
+    if(!user.isVerified) return res.status(401).json({message: "invalid token"})
+    const isValid= await bcrypt.compare(password,user.password )  //compara la contrasena sin encriptar con la controsena encriptada
+    if(!isValid) return res.status(401).json({message: "invalid token"})
+    const token = jwt.sign({user}, process.env.TOKEN_SECRET, {expiresIn:'5d'})
+    return res.json({user, token})
+});
+
+const getLogedUser = catchError(async(req, res) => {
+    return res.json(req.user)
+});
+
+const resetPassword = catchError(async (req, res) => {
+    const { email, frontBaseUrl } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(401).json({ message: "Invalid email" });
+  
+    const code = require('crypto').randomBytes(32).toString('hex');
+    const link = `${frontBaseUrl}/reset_password/${code}`;
+  
+    await sendEmail({
+      to: email,
+      subject: 'Reset your password',
+      html: `
+        <h1 style="color: gray;">Hola ${user.firstName},</h1>
+        <p>Please click on the following link to reset your password:</p>
+        <a href="${link}">${link}</a>
+      `,
+    });
+  
+    await EmailCode.create({ code, userId: user.id });
+    return res.status(201).json({ message: 'Reset password email sent' });
+  });
+
+  const resetPasswordConfirm = catchError(async (req, res) => {
+     const { code } = req.params;
+  const { password } = req.body;
+  const emailCode = await EmailCode.findOne({ where: { code } });
+  if (!emailCode) return res.status(401).json({ message: 'Invalid or expired code' });
+  const user = await User.findOne({ where: { id: emailCode.userId } });
+  if (!user) return res.status(401).json({ message: 'User not found' });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await user.update({ password: hashedPassword });
+  await emailCode.destroy();
+  return res.status(200).json({ message: 'Password reset successfully' });
+  });
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update,
+    verifyEmail,
+    login,
+    getLogedUser,
+    resetPassword, 
+    resetPasswordConfirm
+   
+
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const userRouter = require('./user.router');
 const router = express.Router();
+const sendEmail = require('../utils/senEmail')
 
 // colocar las rutas aquí
+router.use('/users', userRouter)
+
 
 
 module.exports = router;
